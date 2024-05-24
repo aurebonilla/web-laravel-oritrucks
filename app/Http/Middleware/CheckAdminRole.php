21ee2dc6a3faf488ca68bf175014b18597b72fea
aurebonilla <aurebonilla00@gmail.com>
2024-05-24 18:09:04 +0200

    viajeseeder?

--- app/Http/Middleware/CheckAdminRole.php
+++ app/Http/Middleware/CheckAdminRole.php
@@ -10,8 +10,6 @@ class CheckAdminRole
     public function handle($request, Closure $next)
     {
         if (!Auth::check() || Auth::user()->rol != 'admin') {
-            // Si el usuario no está autenticado o no tiene el rol de administrador,
-            // redirigir a la página de inicio.
             Auth::logout();
             return redirect('login');
         }
--- database/seeders/ViajeSeeder.php
+++ database/seeders/ViajeSeeder.php
@@ -2,11 +2,34 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Str;
-use Illuminate\Support\Facades\DB;
+use App\Models\Viaje;
+use App\Models\Conductor;
+use App\Models\Cliente;
+use App\Models\Vehiculo;
 
 class ViajeSeeder extends Seeder
 {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('viajes')->delete();
+        DB::table('viajes')->insert([
+            'identificador' => 'VJ001',
+            'fecha' => 10,
+            'duracion' => '5',
+            'origen' => 'Madrid',
+            'destino' => 'Francia',
+            'km' => 100,
+            'precio' => 100.00,
+            'tarifa' => 'estandar',
+            'vehiculo_id' => '0962 KLM',
+            'conductor_id' => '06332250T',
+            'cliente_dni' => '12345678A',
+        ]);
+    }
 }
